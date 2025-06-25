475a67c54d46a86d9fc394bd80cf3f5cd355c11a
Vitaliy Lytonin <lytonin.v@gmail.com>
2025-06-25 23:48:30 +0500

    add slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import 'normalize.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './styles/main.scss';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+});
