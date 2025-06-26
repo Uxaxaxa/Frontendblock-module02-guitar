4dee73952860de64df43a8c577a49d5e2da53609
Vitaliy Lytonin <lytonin.v@gmail.com>
2025-06-26 17:45:42 +0500

    add slider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import 'normalize.css'
+/* import 'normalize.css' */
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -7,6 +7,12 @@ import './styles/main.scss';
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.slider-teacher', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
   modules: [Navigation, Pagination],
 });
